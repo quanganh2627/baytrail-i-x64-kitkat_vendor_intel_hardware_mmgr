f3113ef2d1d0f46469f5a324810e9e4f0c96c122
Arun Ravindran <arun.ravindran@intel.com>
2013-08-19 16:20:24 +0300

    Revert "[PDK MR2 Temp] disable mmgr test"
    BZ:131755
    
    This reverts commit 461c3c999ee77ba5dae24c92ca2981d3c3560dd3.
    
    Change-Id: I7adf621dc4d0136da6e54bc448aacdf0cea539b4
    Signed-off-by: Arun Ravindran <arun.ravindran@intel.com>
    Reviewed-on: http://android.intel.com:8080/126129
    Tested-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

---
+++ tests/c/Android.mk
@@ -0,0 +1,35 @@
+#############################################
+# MODEM MANAGER C appliation test
+#############################################
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := mmgr-test
+LOCAL_CFLAGS += -Wall -DSTDIO_LOGS -Wvla
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/../../service \
+    $(TARGET_OUT_HEADERS)/IFX-modem \
+    $(TARGET_OUT_HEADERS) \
+    vendor/intel/external/glib/ \
+    vendor/intel/external/glib/android/ \
+    vendor/intel/external/glib/glib/ \
+    ../inc
+
+LOCAL_SRC_FILES:= \
+    ../../service/at.c \
+    ../../service/config.c \
+    ../../service/file.c \
+    ../../service/property.c \
+    ../../service/tty.c \
+    mmgr_test.c \
+    test_cases.c \
+    test_utils.c
+LOCAL_MODULE_TAGS := optional tests
+LOCAL_STATIC_LIBRARIES := libcutils libc
+LOCAL_SHARED_LIBRARIES := libglib-2.0 libmmgrcli
+LOCAL_LDLIBS += -lpthread
+#uncomment this to enable gcov
+#LOCAL_CFLAGS += -fprofile-arcs -ftest-coverage
+#LOCAL_LDFLAGS += -fprofile-arcs -ftest-coverage -lgcov
+include $(BUILD_EXECUTABLE)
+
---
+++ tests/java/Android.mk
@@ -0,0 +1,23 @@
+#############################################
+# MODEM MANGER java appliation test
+#############################################
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional tests
+
+LOCAL_STATIC_JAVA_LIBRARIES := com.intel.internal.telephony.MmgrClient \
+                               android-support-v4
+
+# Only compile source java files in this apk.
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := MMGR_test
+
+LOCAL_CERTIFICATE := platform
+
+LOCAL_PROGUARD_ENABLED := disabled
+
+include $(BUILD_PACKAGE)
+
