6753e7954c7ae7b2c5345dce6e8b87dd2964722e
Sylvain Bourdos <sylvain.bourdos@intel.com>
2013-10-09 12:06:23 +0200

    scalability: add mmgr_saltbay_pr2_7260.xml configuration
    BZ: 138540
    
    This combination is needed for Moorefield pre-silicon
    integration on Saltbay PR2 + XMM7260. It matches
    XMM7260_CONF_2 scalability configuration.
    
    Change-Id: Id3d9fae660564e394e035898abc2e579e04feea0
    Signed-off-by: Sylvain Bourdos <sylvain.bourdos@intel.com>
    Reviewed-on: http://android.intel.com:8080/137180
    Reviewed-by: De Oliveira, CesarX <cesarx.de.oliveira@intel.com>
    Reviewed-by: Regnier, PhilippeX <philippex.regnier@intel.com>
    Reviewed-by: Ravindran, Arun <arun.ravindran@intel.com>
    Tested-by: Louis, NicolasX <nicolasx.louis@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- xml/merrifield/Android.mk
+++ xml/merrifield/Android.mk
@@ -17,11 +17,20 @@ LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := mmgr_saltbay_pr2_7260.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/telephony
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := mmgr_merrifield_xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES :=\
     mmgr_saltbay_pr1.xml\
     mmgr_saltbay_pr2.xml\
+    mmgr_saltbay_pr2_7260.xml\
 
 include $(BUILD_PHONY_PACKAGE)
 
